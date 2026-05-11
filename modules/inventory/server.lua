bc78a11b3075ce495126fcaefedecb5de6d09852
Linden <65407488+thelindat@users.noreply.github.com>
2026-05-12 09:39:49 +1000

    fix(server/inventory): SetSlot return value on item deletion

--- modules/inventory/server.lua
+++ modules/inventory/server.lua
@@ -376,6 +376,7 @@ local Items = require 'modules.items.server'
 ---@param count number
 ---@param metadata any
 ---@param slot number
+---@return SlotWithItem | boolean, string?
 function Inventory.SetSlot(inv, item, count, metadata, slot)
 	inv = Inventory(inv) --[[@as OxInventory]]
 
@@ -411,7 +412,7 @@ function Inventory.SetSlot(inv, item, count, metadata, slot)
 	inv.items[slot] = currentSlot
 	inv.changed = true
 
-	return currentSlot
+	return currentSlot or true
 end
 
 CreateThread(function()
