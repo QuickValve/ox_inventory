1b27e6dc22593a63a4fcb580ff39c917ebed7573
Linden <65407488+thelindat@users.noreply.github.com>
2026-05-11 18:00:30 +1000

    fix(client): don't set coords on default inventory

--- client.lua
+++ client.lua
@@ -294,7 +294,7 @@ function client.openInventory(inv, data)
         }
     })
 
-    if not currentInventory.coords and inv ~= 'container' then
+    if inv and not currentInventory.coords and inv ~= 'container' then
         currentInventory.coords = GetEntityCoords(playerPed)
     end
 
